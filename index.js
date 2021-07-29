e242bc0ac0e619f7e4693dff2ad051fdaa23b1a2
alxtate <83983608+alxtate@users.noreply.github.com>
2021-07-29 08:28:42 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
+import 'react-native-gesture-handler';
 import { registerRootComponent } from 'expo';
-import { AppRegistry, Platform } from 'react-native';
+
 import App from './App';
-import { name as appName } from "./app.json";
-import registerComponenet from 'react-native-elements'
+
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in the Expo client or in a native build,
+// It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-if (Platform.OS === "android") {
-    registerRootComponent(App);
-}
+registerRootComponent(App);
---
+++ navigation/Navigator.js
@@ -0,0 +1,24 @@
+/*import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function HomeScreen() {
+    return (
+        <View>
+            <Text>Home Screen</Text>
+        </View>
+    )
+}
+
+const TabNav = createBottomTabNavigator();
+
+export default function AppScreens() {
+    return (
+        <NavigationContainer>
+            <TabNav.Navigator>
+                <TabNav.Screen name="Home" component={HomeScreen}/>
+            </TabNav.Navigator>
+        </NavigationContainer>
+    )
+}*/
